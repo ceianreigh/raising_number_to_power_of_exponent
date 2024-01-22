d7af573fd438b022152979ebe8107d16f6aef35a
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 22:06:50 +0800

    Ask user for the base and exponent and print the result

--- raising_number_to_power_of_exponent.py
+++ raising_number_to_power_of_exponent.py
@@ -7,5 +7,9 @@
 # return base raised to the power of exp
 
 # ask user for input for base and exp
+base = int(input("Enter the base: "))
+exp = int(input("Enter the exponent: "))
 
 # print the result of the function call
+result = exponent(base, exp)
+print(base, "raised to the power of", exp, "is", result)
