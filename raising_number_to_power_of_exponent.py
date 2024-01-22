7114ddbc9ee8610c957892b09b80592958c20dc2
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 22:02:44 +0800

    Write the instruction and pseudocode

---
+++ raising_number_to_power_of_exponent.py
@@ -0,0 +1,11 @@
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+# pseudocode
+
+# define a function called exponent that takes in two parameters, base and exp
+
+# return base raised to the power of exp
+
+# ask user for input for base and exp
+
+# print the result of the function call
